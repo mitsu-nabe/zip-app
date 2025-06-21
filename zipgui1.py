35de4eca6274e356b25b5a5c6a6ccf06da3eba93
handlove55 <handlove55@gmail.com>
2025-06-21 11:21:27 +0900

    初めてのコミット

---
+++ zipgui1.py
@@ -0,0 +1,58 @@
+import requests
+import tkinter as tk
+
+class ZipSearch(tk.Frame):
+	zipapi = "https://zipcloud.ibsnet.co.jp/api/search"
+	
+	def __init__(self, master=None):
+		super().__init__(master, bg="lightblue", padx=20, pady=20)
+		self.pack(fill=tk.BOTH)
+
+		# 検索用フレーム
+		self.search_frame = tk.Frame(self, pady=10, padx=5)
+		self.search_frame.pack()
+
+		# 入力テキスト（郵便番号）用変数
+		self.zip_entry_var = tk.StringVar()
+
+		# 入力テキスト用 Entry
+		self.zip_entry = tk.Entry(self.search_frame,
+					textvariable=self.zip_entry_var
+					)
+		self.zip_entry.pack(side='left')
+
+		# 「検索」ボタン
+		self.search_button = tk.Button(self.search_frame,
+						text="検索",
+						command=self.search)
+		self.search_button.pack(side='left')
+
+		# 結果表示用のラベル
+		self.result_label = tk.Label(self, bg="lightblue", font=("", 18))
+		self.result_label.pack(fill=tk.X)
+	
+	def search(self):
+		zipcode = self.zip_entry_var.get()
+
+		# 郵便番号APIを呼び出してJSONデータを取得する
+		params = {"zipcode": zipcode}
+		result = requests.get(ZipSearch.zipapi, params=params).json()
+
+		# ステータスコードのチェック
+		if result["status"] != 200:
+			self.result_label["text"] = result["message"]
+		else:
+			# 住所が見つかったかどうかをチェック
+			if result["results"]:
+				# 結果をラベルに表示
+				self.result_label["text"] = result["results"][0]["address1"] + result["results"][0]["address2"] + result["results"][0]["address3"]
+			else:
+				self.result_label["text"] = "見つかりません"
+
+if __name__ == '__main__':
+	root = tk.Tk()
+	root.title('郵便番号検索 API')
+	root.geometry("480x150")
+	root["bg"] = "lightblue"
+	app = ZipSearch(master=root)
+	root.mainloop()
